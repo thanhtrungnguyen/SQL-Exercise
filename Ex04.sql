3dac50fd9cedc9f0e389e1880f8e6b3a2b75780c
Thanh Trung Nguyen <thanhtrung692k@gmail.com>
2023-03-10 10:30:43 +0700

    update ex4

--- Ex04.sql
+++ Ex04.sql
@@ -8,13 +8,14 @@ For example, with an account called Accounts Payable, the sum of France is 14988
 Note: arrange in A-Z order of AccountDescription
 */
 
-SELECT TOP (1000)
-	DimAccount.AccountDescription
-      , [OrganizationKey]
-      , [DepartmentGroupKey]
-      , FactFinance.[AccountKey]
-      , [Amount]
-FROM FactFinance
-	LEFT JOIN DimAccount ON FactFinance.AccountKey = DimAccount.AccountKey
-ORDER BY DimAccount.AccountDescription
 
+
+SELECT DimAccount.AccountDescription
+      , SUM(CASE WHEN DimOrganization.OrganizationName = 'France' THEN FactFinance.Amount END ) AS France
+      , SUM(CASE WHEN DimOrganization.OrganizationName = 'Germany' THEN FactFinance.Amount END ) AS Germany
+	, SUM(CASE WHEN DimOrganization.OrganizationName = 'Australia' THEN FactFinance.Amount END ) AS Australia
+FROM FactFinance
+      LEFT JOIN DimAccount ON FactFinance.AccountKey = DimAccount.AccountKey
+      LEFT JOIN DimOrganization ON FactFinance.OrganizationKey=DimOrganization.OrganizationKey
+GROUP BY DimAccount.AccountDescription
+ORDER BY DimAccount.AccountDescription
